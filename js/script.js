f8b176d44d5489bef79e2a90269f61f0fb544e70
Nikolay Antonovich <TesaloNick@gmail.com>
2022-08-17 14:58:49 +0300

    add closing of columns

--- js/script.js
+++ js/script.js
@@ -11,10 +11,13 @@ class Trello {
     this.printTasks(this.tasks)
     this.surface.addEventListener('mousedown', (e) => this.catchItem(e))
     this.surface.addEventListener('submit', (e) => this.addTask(e))
-    this.surface.addEventListener('click', (e) => this.closeTask(e))
-    this.surface.addEventListener('click', (e) => this.changeTask(e))
+    this.surface.addEventListener('click', (e) => {
+      this.closeTask(e)
+      this.changeTask(e)
+      this.addInputForChanging(e)
+      this.closeColumn(e)
+    })
     this.surface.addEventListener('keypress', (e) => this.changeTask(e))
-    this.surface.addEventListener('click', (e) => this.addInputForChanging(e))
     this.surface.addEventListener('dblclick', (e) => this.addInputForChanging(e));
     this.surface.addEventListener('submit', (e) => this.addColumn(e))
   }
@@ -31,7 +34,10 @@ class Trello {
       columnBox.classList.add('surface__column')
       columnBox.id = column.id
       columnBox.innerHTML = `
-      <div class="surface__head">${column.head}</div>
+      <div class="head">
+        <div class="head__content">${column.head}</div>
+        <div class="head__close"></div>
+      </div>
       <div class="list">
         ${column.tasks.map(item => `
           <div class='task' id=${item.id}>
@@ -63,6 +69,14 @@ class Trello {
       })
       this.counterColumns++
       localStorage.setItem('counterColumns', JSON.stringify(this.counterColumns))
+      this.printTasks(this.tasks)
+      console.log(this.tasks);
+    }
+  }
+
+  closeColumn(e) {
+    if (e.target.closest('.head__close')) {
+      this.tasks = this.tasks.filter(column => column.id !== e.target.closest('.surface__column').id)
       this.printTasks(this.tasks)
       console.log(this.tasks);
     }
